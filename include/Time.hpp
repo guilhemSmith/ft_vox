f4acc9583d23cac62b670b61fb6371ceea62e91b
guilhem smith <smith.guilhem@gmail.com>
2020-10-15 17:20:49 +0200

    Time class mesuring delta between frames, and fps.

---
+++ include/Time.hpp
@@ -0,0 +1,23 @@
+#ifndef TIME_HPP
+# define TIME_HPP
+
+class Time
+{
+private:
+	unsigned int	_delta_time;
+	unsigned int	_last_frame;
+	unsigned int	_frame;
+	unsigned int	_frames_count;
+	unsigned int	_fps;
+	unsigned int	_delta_sum;
+
+public:
+	Time(void);
+
+	bool			update(void);
+
+	unsigned int	delta_time(void);
+	unsigned int	fps(void);
+};
+
+#endif
---
+++ src/Time.cpp
@@ -0,0 +1,30 @@
+#include "Time.hpp"
+#include <SDL2/SDL.h>
+
+Time::Time(void): _delta_time(0), _last_frame(0), _frame(0), _frames_count(0), _fps(0), _delta_sum(0) {}
+
+bool			Time::update(void) {
+	_frame = SDL_GetTicks();
+	_delta_time = _frame - _last_frame;
+	_last_frame = _frame;
+
+	if (_delta_sum + _delta_time > 1000) {
+		_fps = _frames_count;
+		_frames_count = 1;
+		_delta_sum = _delta_sum + _delta_time - 1000;
+		return true;
+	}
+	else {
+		_frames_count++;
+		_delta_sum += _delta_time;
+		return false;
+	}
+}
+
+unsigned int	Time::delta_time(void) {
+	return _delta_time;
+}
+
+unsigned int	Time::fps(void) {
+	return _fps;
+}
