bca07a76f3a3d96a9648c9e908125ec2d2dcf5b0
guilhem smith <smith.guilhem@gmail.com>
2020-10-15 17:21:32 +0200

    Started adding main loop.
    Calling update on camera and time.
    Printing fps and quit with escape.

--- src/ft_vox.cpp
+++ src/ft_vox.cpp
@@ -1,13 +1,16 @@
 #include "ft_vox.hpp"
+#include "Camera.hpp"
+#include "Time.hpp"
 
-int main(int argc, char *argv[])
-{
-	std::cout << "Hello world !" << std::endl;
-	SDL_Init(SDL_INIT_VIDEO);
-	TTF_Init();
-	TTF_Font *font = TTF_OpenFont("../fonts/Roboto-Regular.ttf", 30);
-
-	SDL_Window *window = SDL_CreateWindow(
+bool	game_init(SDL_Window *&window) {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		return false;
+	}
+	if (TTF_Init() < 0) {
+		SDL_Quit();
+		return false;
+	};
+	window = SDL_CreateWindow(
 		"ft_vox",
 		SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED,
@@ -15,30 +18,46 @@ int main(int argc, char *argv[])
 		480,
 		0
 	);
+	if (window == NULL) {
+		TTF_Quit();
+		SDL_Quit();
+		return false;
+	}
+	return true;
+}
 
-	SDL_Surface *surface = SDL_GetWindowSurface(window);
+void	game_loop(SDL_Window *window) {
+	Camera			cam = Camera();
+	Time			time = Time();
+	
+	SDL_Surface		*surface = SDL_GetWindowSurface(window);
 
-	int img_flag = IMG_INIT_PNG;
-	IMG_Init(img_flag);
-	SDL_Surface *texture = IMG_Load("../textures/dirt.png");
+	while (!cam.should_quit()) {
+		cam.update(time.delta_time());
+		
+		SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x00, 0x00, 0x00));
+		SDL_UpdateWindowSurface(window);
 
-	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x00, 0x00, 0x00));
-	SDL_BlitSurface(texture, NULL, surface, NULL);
+		if (time.update()){
+			std::cout << time.fps() << "fps" << std::endl;
+		}
+	}
+}
 
-	SDL_Color white = {255, 255, 255, 255};
-	std::string value = "Hello world !";
-	SDL_Rect position = {10, 100, 300, 50};
-	SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, value.c_str(), white);
-	SDL_BlitSurface(surfaceMessage, NULL, surface, &position);
-	SDL_FreeSurface(surfaceMessage);
-	SDL_UpdateWindowSurface(window);
+int main(int argc, char *argv[])
+{
+	SDL_Window *window;
 
-	SDL_Delay(3000);
+	if (game_init(window)) {
+		game_loop(window);
 
-	SDL_DestroyWindow(window);
-	TTF_CloseFont(font);
-	TTF_Quit();
-	SDL_Quit();
-
-	return 0;
+		std::cout << "Game Over." << std::endl;
+		TTF_Quit();
+		SDL_Quit();
+		return 0;
+	}
+	else {
+		std::cout << "Game initialization failed." << std::endl;
+		return -1;
+	}
 }
