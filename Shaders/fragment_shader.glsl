bf239e2ebacbcd5c4e88c47949cae96db234e569
Benoit AUDIBERT <baudiber@student.42.fr>
2020-10-25 16:50:16 +0100

    added Sand type block and finished fragment shader

--- Shaders/fragment_shader.glsl
+++ Shaders/fragment_shader.glsl
@@ -13,8 +13,7 @@ uniform sampler2D sand;
 
 void main()
 {
-    if (tex == 1)
-    {
+    if (tex == 2) { //dirt
         if (norm.z < 0)
             FragColor = vec4(0.7f, 0.7f, 0.7f, 1.0f) * texture(grass_side, texcoords);
         else if (norm.z > 0)
@@ -27,5 +26,47 @@ void main()
             FragColor = vec4(0.9f, 0.9f, 0.9f, 1.0f) * texture(grass, texcoords);
         else if (norm.y > 0)
             FragColor = vec4(0.3f, 0.3f, 0.3f, 1.0f) * texture(dirt, texcoords);
+    }
+    else if (tex == 3) { //grass
+        if (norm.z < 0)
+            FragColor = vec4(0.7f, 0.7f, 0.7f, 1.0f) * texture(dirt, texcoords);
+        else if (norm.z > 0)
+            FragColor = vec4(0.7f, 0.7f, 0.7f, 1.0f) * texture(dirt, texcoords);
+        else if (norm.x < 0)
+            FragColor = vec4(0.5f, 0.5f, 0.5f, 1.0f) * texture(dirt, texcoords);
+        else if (norm.x > 0)
+            FragColor = vec4(0.4f, 0.4f, 0.4f, 1.0f) * texture(dirt, texcoords);
+        else if (norm.y < 0)
+            FragColor = vec4(0.9f, 0.9f, 0.9f, 1.0f) * texture(dirt, texcoords);
+        else if (norm.y > 0)
+            FragColor = vec4(0.3f, 0.3f, 0.3f, 1.0f) * texture(dirt, texcoords);
+    }
+    else if (tex == 1) { //rock
+        if (norm.z < 0)
+            FragColor = vec4(0.7f, 0.7f, 0.7f, 1.0f) * texture(rock, texcoords);
+        else if (norm.z > 0)
+            FragColor = vec4(0.7f, 0.7f, 0.7f, 1.0f) * texture(rock, texcoords);
+        else if (norm.x < 0)
+            FragColor = vec4(0.5f, 0.5f, 0.5f, 1.0f) * texture(rock, texcoords);
+        else if (norm.x > 0)
+            FragColor = vec4(0.4f, 0.4f, 0.4f, 1.0f) * texture(rock, texcoords);
+        else if (norm.y < 0)
+            FragColor = vec4(0.9f, 0.9f, 0.9f, 1.0f) * texture(rock, texcoords);
+        else if (norm.y > 0)
+            FragColor = vec4(0.3f, 0.3f, 0.3f, 1.0f) * texture(rock, texcoords);
+    }
+    else if (tex == 4) { //sand
+        if (norm.z < 0)
+            FragColor = vec4(0.7f, 0.7f, 0.7f, 1.0f) * texture(sand, texcoords);
+        else if (norm.z > 0)
+            FragColor = vec4(0.7f, 0.7f, 0.7f, 1.0f) * texture(sand, texcoords);
+        else if (norm.x < 0)
+            FragColor = vec4(0.5f, 0.5f, 0.5f, 1.0f) * texture(sand, texcoords);
+        else if (norm.x > 0)
+            FragColor = vec4(0.4f, 0.4f, 0.4f, 1.0f) * texture(sand, texcoords);
+        else if (norm.y < 0)
+            FragColor = vec4(0.9f, 0.9f, 0.9f, 1.0f) * texture(sand, texcoords);
+        else if (norm.y > 0)
+            FragColor = vec4(0.3f, 0.3f, 0.3f, 1.0f) * texture(sand, texcoords);
     }
 }
