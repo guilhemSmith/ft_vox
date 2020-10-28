7e61f743a9bdfdf29392001bc442f3c04e8f869a
Guilhem SMITH <gsmith@student.42.fr>
2020-10-28 18:18:03 +0100

    World generation create biomes.

--- include/World.hpp
+++ include/World.hpp
@@ -6,17 +6,22 @@
 
 class World {
 private:
-	unsigned int const					_seed;
-	Noise								_heightmap;
-	Noise								_biomes;
-	Noise								_caverns;
+	unsigned int const				_seed;
+	Noise							_heightmap;
+	Noise							_biomes;
+	Noise							_caverns;
+
+	double							_setLayers(std::array<unsigned int, 3>& layers_voxel, unsigned int& mid_layer_size, unsigned int& top_layer_size, double amplitude, double biome) const;
 public:
 	World(unsigned int seed);
 
-	bool								fillChunk(std::array<std::array<std::array<char, 32>, 32>, 32>& voxels, glm::vec3 pos) const;
+	bool							fillChunk(std::array<std::array<std::array<char, 32>, 32>, 32>& voxels, glm::vec3 pos) const;
 
-	static const unsigned int			NOISE_STRETCH;
-	static const unsigned int			NOISE_SIZE;
+	static const unsigned int		NOISE_STRETCH;
+	static const unsigned int		NOISE_SIZE;
+	static const unsigned int		HEIGHT_MID;
+	static const unsigned int		HEIGHT_AMPLITUDE;
+	static const double				BIOME_STEP;
 };
 
 #endif
