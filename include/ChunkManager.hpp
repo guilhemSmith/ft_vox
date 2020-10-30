df948344bc44d0d7dc03894eaeb124b4bdffe16c
guilhem smith <smith.guilhem@gmail.com>
2020-10-30 18:51:26 +0100

    Fix loading with another thread.
    Need checking.

--- include/ChunkManager.hpp
+++ include/ChunkManager.hpp
@@ -6,15 +6,21 @@
 # include <glm/glm.hpp>
 # include <unordered_map>
 # include <vector>
+# include <queue>
+# include <mutex>
+# include <thread>
 
 class ChunkManager {
 private:
-	World									                    _world;
+	World														_world;
 	std::unordered_map<unsigned int, std::shared_ptr<Chunk>>	_chunks_loaded;
-	std::vector<std::weak_ptr<Chunk>>						    _chunks_visible;
-	std::vector<glm::u32vec3>				                    _chunks_to_load;
-	std::vector<glm::u32vec3>				                    _chunks_to_unload;
-	glm::u32vec3							                    _last_cam_chunk;
+	std::vector<std::weak_ptr<Chunk>>							_chunks_visible;
+	std::queue<std::array<glm::u32vec3, 2>>						_chunks_to_load;
+	std::queue<glm::u32vec3>									_chunks_to_unload;
+	glm::u32vec3												_last_cam_chunk;
+	std::mutex													_mtx;
+	bool														_keep_loading;
+	std::thread													_loading_thread;
 
 	unsigned int							                    _chunkIndex(glm::u32vec3 pos) const;
 	void									                    _detectVisibleChunks(glm::vec3 pos, glm::vec3 dir);
@@ -22,15 +28,18 @@ private:
 	void									                    _detectChunkToLoad(glm::u32vec3 cam_chunk_pos);
 	void                                                        _chunkRemesh(glm::vec3);
 
+	void														_loadRoutine(void);
+
 public:
 	ChunkManager(unsigned int seed);
 
 	std::vector<std::weak_ptr<Chunk>>&					        getChunksFromPos(glm::vec3 cam_pos, glm::vec3 cam_dir);
 	void									                    removeChunk(glm::u32vec3 pos);
 
-	static const glm::u32vec3				                    SIZES_VOXELS;
-	static const glm::u32vec3				                    SIZES_CHUNKS;
-	static const float						                    VIEW_DISTANCE;
+	static const glm::u32vec3									SIZES_VOXELS;
+	static const glm::u32vec3									SIZES_CHUNKS;
+	static const float											VIEW_DISTANCE;
+	static const float											LOAD_DISTANCE;
 };
 
 #endif
